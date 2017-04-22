60fc959beee9731b7d3ef1289eba323a4d27ac10
crisskimaryo <crisskimary@gmail.com>
2017-04-23 00:25:01 +0300

    pos update

--- src/app/pos/pos.component.ts
+++ src/app/pos/pos.component.ts
@@ -7,17 +7,44 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class PosComponent implements OnInit {
   @Input() iproducts: any[];
+  @Input() isselected: any = [];
+
+
   constructor() { }
 
   ngOnInit() {
     this.iproducts = [
-      { 'name': 'amoksiklav' },
-      { 'name': 'paracetamol' },
-      { 'name': 'citramon' },
-      { 'name': 'cheston cold' },
-      { 'name': 'brufen' },
-      { 'name': 'combiflam' },
+      { 'id': 1, 'name': 'amoksiklav' },
+      { 'id': 2, 'name': 'paracetamol' },
+      { 'id': 3, 'name': 'citramon' },
+      { 'id': 4, 'name': 'cheston cold' },
+      { 'id': 5, 'name': 'brufen' },
+      { 'id': 6, 'name': 'combiflam' },
     ]
+  }
+
+  itemremove(item) {
+    //remove selected
+    const index = this.isselected.indexOf(item);
+    if (index > -1) {
+      this.isselected.splice(index, 1);
+      console.log(this.isselected);
+    }
+  }
+  itemadd(item) {
+    console.log(item)
+    const index = this.isselected.indexOf(item);
+    console.log(index)
+    if (index === -1) {
+      this.isselected.push(item);
+    }
+    //TODO:check if the item is availabe on selecteditem
+    //current item from product list when selected 
+    // const index = this.iproducts.indexOf(item);
+    // if (index > -1) {
+    //   this.iproducts.splice(index, 1);
+    //   console.log(this.iproducts);
+    // }
   }
 
 }
